c2b6973589d6b37913eabbf32e019cdcc40d8f07
Parth1501 <shethwalaparth2000@gmail.com>
2024-09-23 16:42:16 +0530

    checking last 3 days

--- src/main/java/com/job/scrapper/service/Scrapper.java
+++ src/main/java/com/job/scrapper/service/Scrapper.java
@@ -59,7 +59,9 @@ public class Scrapper {
     }
 
     public List<JobData> scrapGoogleJob() {
-        String url="https://www.google.com/search?client=ubuntu-sn&sca_esv=37afbf05fd6f097e&sca_upv=1&channel=fs&biw=1680&bih=427&q=physiotherapy+job+posting+surat+since+yesterday&uds=ADvngMjcH0KdF7qGWtwTBrP0nt7daSGmSjLTAH-oRzMvJ5-IJgVxBISlWgvCsGRrRw3-Tg0T_JypLLFpTbSoOihNAQFySVbmthZ9GVT3Piu-mTgV5n1gsDnhN5eFo5C52ojzBoJnSjZfJunrrJHFwAy3RcxZZOury3ErVWwsSdhjQJ2-ZISa8-a7x7KSFlv5kVjito9XItGFGH-ahDuC06EVifFBPu8z7Z6hdlafsUNeMJawVkROOPZuO0eex-CSu4FQ2IRcsimK3YWt3n-IOxf937wJTN2na0JSMYZH6H19aJKxGaKPhnTl9MnxjcYBhe-INpgkHXUYPZP7dfj4uSKv-Qvf_xSamg&udm=8&sa=X&ved=2ahUKEwig9MatmdSIAxUxhq8BHcXeB7kQkbEKegQICxAE";
+        // 3 days: https://www.google.com/search?sca_esv=e44d5ba731f3a8e3&sca_upv=1&sxsrf=ADLYWILoDavJgPH35l9dnSJ67vW40YY7lQ%3A1727089159479&q=physiotherapy%20job%20posting%20surat%20in%20the%20last%203%20days&uds=ADvngMjcH0KdF7qGWtwTBrP0nt7dRHZs_3UxVkBT2e_bKJy3gjt2kPnVG89vLBL2NQakbL_Sc4mvUhdhgw4YS5VJXWs7riUgpHk-7HdYx_Gqd1DwHdRUAvWv_hC7LhsVNhiq0C_dqf0SGWiQFtBzetubbaL_czXAbyCpI-W2Rt9lg20undHhgM_pEQYncGY7-ACdnjANRp0_8aBSRp5NJtJS9_zvHRJgLzmiN1p4yVElF1C8b7q9USvERGR7YuYw1pCy6VYntuW20EYpvEu9sqDEzp_z7wqCp_pNdJ04cGH7cNN_ibWG2hlvdMaAgzq24dHB2ck72PIUXkQmPgATxCkLF7PV-j5gqg&sa=X&ved=2ahUKEwjA0JfB9NiIAxVN7TgGHSyAHIEQ3L8LegQIHxAN&jbr=sep:0&udm=8
+        // 1 day: https://www.google.com/search?client=ubuntu-sn&sca_esv=37afbf05fd6f097e&sca_upv=1&channel=fs&biw=1680&bih=427&q=physiotherapy+job+posting+surat+since+yesterday&uds=ADvngMjcH0KdF7qGWtwTBrP0nt7daSGmSjLTAH-oRzMvJ5-IJgVxBISlWgvCsGRrRw3-Tg0T_JypLLFpTbSoOihNAQFySVbmthZ9GVT3Piu-mTgV5n1gsDnhN5eFo5C52ojzBoJnSjZfJunrrJHFwAy3RcxZZOury3ErVWwsSdhjQJ2-ZISa8-a7x7KSFlv5kVjito9XItGFGH-ahDuC06EVifFBPu8z7Z6hdlafsUNeMJawVkROOPZuO0eex-CSu4FQ2IRcsimK3YWt3n-IOxf937wJTN2na0JSMYZH6H19aJKxGaKPhnTl9MnxjcYBhe-INpgkHXUYPZP7dfj4uSKv-Qvf_xSamg&udm=8&sa=X&ved=2ahUKEwig9MatmdSIAxUxhq8BHcXeB7kQkbEKegQICxAE
+        String url="https://www.google.com/search?client=ubuntu-sn&hs=HD6&sca_esv=445c9ec50e6b4223&sca_upv=1&channel=fs&biw=1680&bih=921&q=physiotherapy+job+posting+in+the+last+3+days+near+Surat,+Gujarat+in+the+last+3+days&uds=ADvngMgvwjThEGn1QzCbykcnfc2RaXCi3m_y-OFkWLJlrRuPYhtbhPQ1VJrn2e4W3qcHBMmWlufYdSkeS4YUIf45qQ1KxgkUc24y9H7s0rdnmvoTEqlg244OhgctMpgTTT5ESHcs4s44rr_Viop8wnj-UMcq82lOFxVVRLlCnQqigN0LcDF8BDduDp0zLdSUI5aEalEcOBkmxyuN_BD5Ep2rsU6GpTZf-PL-E0Ss6w98ZW3AAuO3ItkfHKpbG80qg0LtJACDiXSOW4m0bXmLZI8b-oQtqdWxMyTLxVO9cjpD4XC1uhdEl-JGEU2IZeK7cFDzOgGRjU5se79a0Ym5zPpta0nu4uVdVoy0_XO22CEPacDYNfQnvgg&udm=8&sa=X&ved=2ahUKEwjy54uO99iIAxV9n2MGHb2OJx4QkbEKegQIDBAE&jbr=sep:0";
         List<JobData> res=new ArrayList<>();
         try {
             // Fetch the HTML document from the given URL
@@ -68,8 +70,6 @@ public class Scrapper {
             // Example: Get the title of the page
 
             // Example: Select specific elements using CSS selectors
-            var data = document.getElementsByAttributeValue("data-id","jobs-detail-viewer"); // Select all <p> elements
-
             var jobNames=document.select(".tNxQIb.PUpOsf");
             var institute=document.select(".wHYlTd.MKCbgd.a3jPc");
             var locations=document.select(".wHYlTd.FqK3wc.MKCbgd");
--- src/test/java/com/job/scrapper/ScrapperApplicationTests.java
+++ src/test/java/com/job/scrapper/ScrapperApplicationTests.java
@@ -1,13 +1,26 @@
 package com.job.scrapper;
 
+import com.job.scrapper.schuedule.Scheduler;
+import com.job.scrapper.service.Scrapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ScrapperApplicationTests {
 
+	@Autowired
+	Scrapper scrapper;
+
+	@Autowired
+	Scheduler scheduler;
 	@Test
 	void contextLoads() {
+		scrapper.scrapGoogleJob();
+	}
+@Test
+	void runScheduler(){
+		scheduler.runWebScrapping();
 	}
 
 }
